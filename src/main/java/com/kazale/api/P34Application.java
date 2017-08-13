daedffc777faea64bd18c48eb1b9c43c269b1d6f
Marcio <marcio@kazale.com>
2017-08-13 18:59:07 +0100

    Envio inicial dos arquivos

---
+++ src/main/java/com/kazale/api/P34Application.java
@@ -0,0 +1,12 @@
+package com.kazale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P34Application.class, args);
+	}
+}
